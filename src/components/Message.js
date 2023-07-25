1c5014cc59ca831bb92b33a99ce17a8b829c125c
Crystal <muthonigachanja2000@gmail.com>
2023-07-24 22:42:53 -0700

    Complete

---
+++ src/components/Message.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+
+function Message() {
+
+  return (
+
+    <div>
+
+      <h1>
+
+       Hey you wassup
+
+      </h1>
+
+    </div>
+
+  );
+
+}
+
+
+
+
+export default Message;
---
+++ src/components/Signup.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+
+
+function Signup() {
+
+  return (
+
+    <div>
+
+      <h1>
+
+       Welcome.
+
+      </h1>
+
+    </div>
+
+  );
+
+}
+
+
+
+
+export default Signup;
